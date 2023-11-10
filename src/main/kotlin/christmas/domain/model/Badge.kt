e25a0c89742d7290f4a933576a30d36ebc7daa28
윤동현 <donghyunyoon81@gmail.com>
2023-11-10 23:45:40 +0900

    feat: 할인 가격에 따른 뱃지의 종류를 반환하는 기능 추가

---
+++ src/main/kotlin/christmas/domain/model/Badge.kt
@@ -0,0 +1,13 @@
+package christmas.domain.model
+
+class Badge(private val benefitsPrice: Int) {
+    private val type = when {
+        benefitsPrice >= 20_000 -> "산타"
+        benefitsPrice >= 10_000 -> "트리"
+        benefitsPrice >= 5_000 -> "별"
+        else -> "없음"
+    }
+
+    fun getType() = type
+
+}
---
+++ src/test/kotlin/christmas/domain/model/BadgeTest.kt
@@ -0,0 +1,19 @@
+package christmas.domain.model
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class BadgeTest {
+    lateinit var badge: Badge
+
+    @BeforeEach
+    fun `setUp`(){
+        badge = Badge(6000)
+    }
+
+    @Test
+    fun `뱃지가 별이 아니라면 오류가 발생 한다`(){
+        assertEquals(badge.getType() ,"별")
+    }
+}
