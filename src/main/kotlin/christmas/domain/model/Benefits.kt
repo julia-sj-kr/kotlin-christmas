7a9447ca8dda2592529c593526ad586529a26fb9
윤동현 <donghyunyoon81@gmail.com>
2023-11-10 22:08:39 +0900

    refactor: 함수의 이름을 행위에 어울리도록 이름 변경

--- src/main/kotlin/christmas/domain/model/Benefits.kt
+++ src/main/kotlin/christmas/domain/model/Benefits.kt
@@ -1,36 +1,34 @@
 package christmas.domain.model
 
-import christmas.enum.Main
-
 class Benefits(val date: Date, val order: Order) {
 
     val discounts = mutableListOf<Discount>()
 
     init {
-        DDayDiscountPrice()
-        weekdayDiscount()
-        weekendDiscount()
-        specialDiscount()
+        addDDayDiscountPrice()
+        addWeekdayDiscount()
+        addWeekendDiscount()
+        addSpecialDiscount()
     }
 
-    fun DDayDiscountPrice() {
+    private fun addDDayDiscountPrice() {
         if (date.day > 25) return
         discounts.add(Discount("크리스마스 디 데이 할인", 1000 + (date.day - 1) * 100))
     }
 
-    fun weekdayDiscount() {
+    private fun addWeekdayDiscount() {
         if (date.isWeekend) return
         val dessertMenus = order.getMenus().filter { menu -> menu.type == "디저트" }
         discounts.add(Discount("평일 할인", dessertMenus.sumOf { dessert -> dessert.count * 2023 }))
     }
 
-    fun weekendDiscount() {
+    private fun addWeekendDiscount() {
         if (date.isWeekend.not()) return
         val mainMenus = order.getMenus().filter { menu -> menu.type == "메인" }
         discounts.add(Discount("주말 할인", mainMenus.sumOf { main -> main.count * 2023 }))
     }
 
-    fun specialDiscount(){
+    private fun addSpecialDiscount(){
         if (date.hasStar.not()) return
         discounts.add(Discount("특별 할인", 1000))
     }
