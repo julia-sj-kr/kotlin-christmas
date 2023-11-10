1a2e0618adb9892deb31ad69a616b5f1ac416768
윤동현 <donghyunyoon81@gmail.com>
2023-11-10 21:07:24 +0900

    feat: 메뉴를 반환하는 기능 추가

--- src/main/kotlin/christmas/domain/model/Order.kt
+++ src/main/kotlin/christmas/domain/model/Order.kt
@@ -2,5 +2,6 @@ package christmas.domain.model
 
 class Order(private val menus: List<Menu>) {
 
+    fun getMenus() = menus
     fun getTotalPrice() = menus.sumOf { menu -> menu.count * menu.price }
 }
