420eef871840bc7097e03ec6500f7f86c700c8bc
윤동현 <donghyunyoon81@gmail.com>
2023-11-11 00:08:53 +0900

    feat: 주문할 메뉴를 입력 받는 기능 구현

--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -4,11 +4,21 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun readDate(): Int {
-        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+        println("12월 중 식당 예상 방문 날짜는 언제인가요? (12월 날짜에 포함 되는 1에서 31중에 입력해 주세요!)")
         val input = Console.readLine()
         val day = requireNotNull(input.toIntOrNull())
         require((1..31).contains(day))
         return day
     }
+//주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)
 
+    fun readMenus(): List<Pair<String, Int>> {
+        println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+        val input = Console.readLine()
+        val menus = input.split(",").map { menu ->
+            val (name, count) = menu.split("-")
+            Pair(name, requireNotNull(count.toIntOrNull()))
+        }
+        return menus
+    }
 }
