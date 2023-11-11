afb669b931e85427bc959760fdd31f149c6d5372
윤동현 <donghyunyoon81@gmail.com>
2023-11-11 16:49:18 +0900

    feat: 주문 메뉴를 보여주는 기능 추가

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -1,9 +1,18 @@
 package christmas.view
 
+import christmas.domain.model.Menu
+
 class OutputView {
 
     fun introduce(){
         println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
     }
+
+  fun showOrderMenus(menus: List<Menu>){
+      println("<주문 메뉴>")
+      menus.forEach { menu ->
+          println("${menu.name} ${menu.count}개")
+      }
+  }
 
 }
