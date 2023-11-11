2db34b833a15cb3ae9cb105bb8d9f3b294c38028
윤동현 <donghyunyoon81@gmail.com>
2023-11-11 17:19:59 +0900

    feat: 할인 전 총주문 금액을 보여주는 기능 구현

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -1,18 +1,27 @@
 package christmas.view
 
 import christmas.domain.model.Menu
+import christmas.domain.model.Order
 
 class OutputView {
 
-    fun introduce(){
+    fun introduce() {
         println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
     }
 
-  fun showOrderMenus(menus: List<Menu>){
-      println("<주문 메뉴>")
-      menus.forEach { menu ->
-          println("${menu.name} ${menu.count}개")
-      }
-  }
+    fun showOrderMenus(menus: List<Menu>) {
+        println("<주문 메뉴>")
+        menus.forEach { menu ->
+            println("${menu.name} ${menu.count}개")
+        }
+    }
+
+    fun showTotalPrice(order: Order) {
+        println("<할인 전 총주문 금액>")
+        val totalPrice = order.getTotalPrice()
+        println(formatCurrency(totalPrice))
+    }
+
+    private fun formatCurrency(price: Int) = String.format("%,.0f원", price)
 
 }
