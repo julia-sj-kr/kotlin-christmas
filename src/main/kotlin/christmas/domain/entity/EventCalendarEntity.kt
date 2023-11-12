b562a37cd745413f5d67fb6a8ad92cb02d0e89a3
윤동현 <donghyunyoon81@gmail.com>
2023-11-12 14:17:18 +0900

    refactor: 이벤트 달력의 날짜 데이터 생성을 함수로 분리

--- src/main/kotlin/christmas/domain/entity/EventCalendarEntity.kt
+++ src/main/kotlin/christmas/domain/entity/EventCalendarEntity.kt
@@ -5,12 +5,10 @@ import christmas.presentation.model.DateModel
 class EventCalendarEntity {
     val dates = List(30) { index ->
         val day = index + 1
-        return@List when {
-            isWeekend(day) -> DateModel(day, isWeekend = true, hasStar = false)
-            hasStar(day) -> DateModel(day, isWeekend = false, hasStar = true)
-            else -> DateModel(day, isWeekend = false, hasStar = false)
-        }
+        createDateModel(day)
     }
+
+    private fun createDateModel(day: Int) = DateModel(day, isWeekend(day), hasStar(day))
 
     private fun isWeekend(day: Int) = day % 7 == 1 || day % 7 == 2
 
---
+++ src/test/kotlin/christmas/domain/entity/EventCalendarTest.kt
@@ -0,0 +1,26 @@
+package christmas.domain.entity
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class EventCalendarTest {
+
+    lateinit var eventCalendarEntity:EventCalendarEntity
+
+    @BeforeEach
+    fun `setUp`(){
+        eventCalendarEntity = EventCalendarEntity()
+    }
+
+
+    @ValueSource(ints = [25,3,10])
+    @ParameterizedTest
+    fun `캘린더의 일요일과 크리스마스 당일이 별이 없을 경우 오류가 발생 한다`(day:Int){
+        val index = day-1
+        val hasStar = eventCalendarEntity.dates[index].hasStar
+        assert(hasStar)
+    }
+
+}
