fc3153f12ceefc8cf61a82fd3813d9ec7f1deced
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-29 19:44:41 +0900

    feat(OutputView): 주문 메뉴들 출력 기능 추가

--- src/main/kotlin/christmas/OutputView.kt
+++ src/main/kotlin/christmas/OutputView.kt
@@ -2,4 +2,11 @@ package christmas
 
 class OutputView {
     fun printStart() = println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
+
+    fun printOrderMenus(orderMenus: List<OrderMenu>) {
+        println("<주문 메뉴>")
+        orderMenus.forEach { orderMenu ->
+            println(orderMenu.menu.menu + "${orderMenu.count}개")
+        }
+    }
 }
