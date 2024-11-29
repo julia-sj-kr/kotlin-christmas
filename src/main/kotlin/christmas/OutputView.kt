82014b7df1c14301cb773a1c157a40a4107822c2
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-29 19:50:43 +0900

    feat(OutputView): 할인 전 총주문 금액 출력 기능 추가

--- src/main/kotlin/christmas/OutputView.kt
+++ src/main/kotlin/christmas/OutputView.kt
@@ -1,5 +1,7 @@
 package christmas
 
+import java.text.DecimalFormat
+
 class OutputView {
     fun printStart() = println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
 
@@ -8,5 +10,16 @@ class OutputView {
         orderMenus.forEach { orderMenu ->
             println(orderMenu.menu.menu + "${orderMenu.count}개")
         }
+    }
+
+    fun printTotalOrderPrice(orderMenus: List<OrderMenu>) {
+        val totalOrderPrice = orderMenus.sumOf { it.menu.price * it.count }
+        println("<할인 전 총주문 금액>")
+        println(totalOrderPrice.toWonFormat())
+    }
+
+    private fun Int.toWonFormat(): String {
+        val formatter = DecimalFormat("#,###원")
+        return formatter.format(this)
     }
 }
