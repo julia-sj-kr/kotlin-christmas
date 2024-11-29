b4f4fbc2e5b8aaea8a28ca6a6b5799e905c9ab6e
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-29 20:57:02 +0900

    feat(OutputView): 증정 메뉴 출력 기능 추가

--- src/main/kotlin/christmas/OutputView.kt
+++ src/main/kotlin/christmas/OutputView.kt
@@ -8,13 +8,19 @@ class OutputView {
     fun printOrderMenus(orderMenus: List<OrderMenu>) {
         println("<주문 메뉴>")
         orderMenus.forEach { orderMenu ->
-            println(orderMenu.menu.menu + "${orderMenu.count}개")
+            println(orderMenu.menu.menu + " ${orderMenu.count}개")
         }
     }
 
     fun printTotalOrderPrice(totalOrderPrice: Int) {
         println("<할인 전 총주문 금액>")
         println(totalOrderPrice.toWonFormat())
+    }
+
+    fun printApply(hasApplyMenu: Boolean) {
+        if (hasApplyMenu.not()) return
+        println("<증정 메뉴>")
+        println(Menu.CHAMPAGNE.menu + " ${Menu.CHAMPAGNE.price}개")
     }
 
     private fun Int.toWonFormat(): String {
