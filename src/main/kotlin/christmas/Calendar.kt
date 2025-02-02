e3473dcef6e649016bbe047fe3a9a3528883f92e
julia-sj-kr <5637343@hanmail.net>
2025-02-02 18:58:53 +0900

    feat(Discount): 이벤트 내용들을 함수로 구현

---
+++ src/main/kotlin/christmas/Calendar.kt
@@ -0,0 +1,18 @@
+package christmas
+
+class Calendar {
+    val days = (1..31).toList() //반환 타입 List<Int>
+    //val days = listOf(1..31) //반환 타입 List<IntRange>
+
+//    금,토 : days를 나누기 했을때 나머지가 0 또는 1
+//    일,월,화,수,목: days를 나누기 했을때 나머지가 2 이상 6 이하
+
+    fun sortWeekend(day: Int): Boolean {
+        return day % 7 == 0 || day % 7 == 1 //true이면 주말, false이면 평일
+    }
+
+    fun checkSpecialDays(day:Int):Boolean{
+        val specialDays = listOf(3,10,17,24,25,31)
+        return specialDays.contains(day)
+    }
+}
---
+++ src/main/kotlin/christmas/Discount.kt
@@ -0,0 +1,70 @@
+package christmas
+
+import christmas.MenuBoard.category
+import christmas.MenuBoard.menuItems
+
+class Discount {
+//    크리스마스 디데이 할인
+//    이벤트 기간: 2023.12.1 ~ 2023.12.25
+//    1,000원으로 시작하여 크리스마스가 다가올수록 날마다 할인 금액이 100원씩 증가
+//    총주문 금액에서 해당 금액만큼 할인
+//    (e.g. 시작일인 12월 1일에 1,000원, 2일에 1,100원, ..., 25일엔 3,400원 할인)
+//
+//    평일 할인(일요일~목요일): 평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인
+//
+//    주말 할인(금요일, 토요일): 주말에는 메인 메뉴를 메뉴 1개당 2,023원 할인
+//
+//    특별 할인: 이벤트 달력에 별이 있으면 총주문 금액에서 1,000원 할인
+//
+//    증정 이벤트: 할인 전 총주문 금액이 12만 원 이상일 때, 샴페인 1개 증정
+//
+//    이벤트 기간: '크리스마스 디데이 할인'을 제외한 다른 이벤트는 2023.12.1 ~ 2023.12.31 동안 적용
+
+
+    fun christmasDiscount(date: Int): Int {
+        return if (date in 1..25) 1000 + 100 * (date - 1)
+        else 0
+    }
+
+    fun weekDayDiscount(date: Int, orderList: List<Order>): Int {
+        var dessertDiscount: Int = 0
+        ////true이면 주말, false이면 평일
+        if (!Calendar().sortWeekend(date)) {
+            orderList.forEach { order ->
+                //val category = MenuBoard.category
+                if (category["디저트"]?.contains(order.name) == true) {
+                    dessertDiscount += 2023
+                }
+            }
+        }
+        return dessertDiscount
+    }
+
+    //함수형 스타일
+    fun weekendDiscount(date: Int, orderList: List<Order>): Int {
+        var mainDiscount: Int = 0
+        if (Calendar().sortWeekend(date)) {
+            mainDiscount = orderList
+                .filter { order -> category["main"]?.contains(order.name) == true }
+                .size * 2023
+        }
+        return mainDiscount
+    }
+
+    fun specialDayDiscount(date: Int): Int {
+        return if (Calendar().checkSpecialDays(date)) 1000
+        else 0
+    }
+
+    fun getChampagne(orderList: MutableList<Order>): Int {
+        var totalPrice = 0
+        orderList.forEach { order ->
+            val price = menuItems[order.name]!!.toInt()
+            totalPrice += price * order.num
+        }
+
+        if(totalPrice>120000) orderList.add(Order("샴페인",1))
+        return totalPrice
+    }
+}
+
